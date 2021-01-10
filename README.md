96aa7da0ab8979ccbe358749f7909185911febbc
chenzl <101du@163.com>
2021-01-10 14:50:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### How to deploy
+```
+npm install --save express
+npm install --save-dev babel-cli babel-preset-es2015 rimraf
+npm install @uniswap/sdk
+```
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { ChainId } from '@uniswap/sdk'
 
 const app = express()
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-	  console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!')
+	console.log(`The chainId of mainnet is ${ChainId.MAINNET}.`)
 })
